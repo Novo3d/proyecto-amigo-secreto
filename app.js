fb8fbce703a743b8dc48ea455ed81826f547ecf5
Sebastian Novoa P. <info.novo3d@gmail.com>
2025-08-06 19:46:41 -0400

    creación codigo de js funciones agregarAmigo y listaAmigos

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//En este arreglo vacio se van ingresar los nombres recibidos
+let amigos = [];
+
+//En esta función se muestra el contenido del arreglo en el HTML
+function listaDeAmigos(){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (const nombre of amigos){
+        const item = document.createElement("li");
+        item.textContent = nombre;
+        lista.appendChild(item);
+    }
+    //con este console.log puedo ir viendo como el array se va poblando
+    //console.log(amigos);
+}
+
+//Aquí va a ir el obtener el imput y el botón con sus IDs
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreNuevo = inputAmigo.value.trim();
+
+    if (nombreNuevo !== ""){
+        //Aquí se agrega el nombre del arreglo
+        amigos.push(nombreNuevo);
+
+        //Aquí se limpia el campo de texto
+        inputAmigo.value ="";
+
+        //Aquí se llama a la función para actualizar la lista en el HTML listaDeAmigos()
+        listaDeAmigos();
+    }  else {
+        //Se muestra un alerta si el campo está vacío
+        alert("Por favor, inserte un nombre");
+    }
+}
+
